833ca1f868dca266e5c87f12f8b0c68f1f53d826
Jemal Ebrahim M Sadik <Jemal.e.m.sadik@gmail.com>
2024-04-27 16:05:40 -0500

    Added methods getAllUsers, CreateUser, updateUser and deleteUser

--- backend/src/main/java/com/revature/controllers/UserController.java
+++ backend/src/main/java/com/revature/controllers/UserController.java
@@ -1,4 +1,57 @@
 package com.revature.controllers;
 
+import com.revature.models.Reimbursement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import com.revature.models.User;
+import com.revature.services.UserService;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+       this.userService = userService;
+    }
+
+    // get all users (manager)
+    @GetMapping("")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUser();
+
+        // TODO: check if list is empty and return appropriate message
+
+        return ResponseEntity.ok(users);
+    }
+
+    @PostMapping("/{role}")
+    public ResponseEntity <User> createUser(@RequestBody User user, @PathVariable String role){
+        User savedUser = userService.createUser(user,role);
+        // TODO: error handling
+
+        return ResponseEntity.ok(savedUser);
+    }
+
+    @PatchMapping("/{userid}")
+    public ResponseEntity <User> updateUser(@RequestBody String role, @PathVariable int userid){
+        User savedUser = userService.updateUser(role, userid);
+        // TODO: error handling
+        return ResponseEntity.ok(savedUser);
+    }
+
+    @DeleteMapping("/{userid}")
+    public ResponseEntity <User> deleteUser(@PathVariable int uderid) {
+        User deletedUser = userService.deleteUser(uderid);
+        // TODO: error handling
+        return ResponseEntity.ok(deletedUser);
+    }
+
 }
+
--- backend/src/main/java/com/revature/services/UserService.java
+++ backend/src/main/java/com/revature/services/UserService.java
@@ -1,4 +1,66 @@
 package com.revature.services;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.revature.enums.Roles;
+import com.revature.models.User;
+import com.revature.daos.UserDAO;
+
 public class UserService {
+
+    private UserDAO userDAO;
+
+    @Autowired
+    public UserService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public List<User> getAllUser() {
+        // TODO: get logged in user
+        /*
+        if the logged in user has a wrong Role, throw exception
+        if user is managerRole return all users
+        */
+        return userDAO.findAll();
+    }
+
+    public User createUser(User user, String role) {
+        String roleDesc = Roles.valueOf(role).getDescription();
+        if (!roleDesc.isEmpty()) {
+            user.setRole(roleDesc);
+            // TODO
+            return userDAO.save(user);
+        }
+        // TODO: Throw an exception
+        return new User();
+    }
+
+    public User updateUser(String role, int userid) {
+        Optional<User> checkUser = userDAO.findById(userid);
+        if (checkUser.isPresent()) {
+            String roleDesc = Roles.valueOf(role).getDescription();
+            if (!roleDesc.isEmpty()) {
+                User user = checkUser.get();
+                user.setRole(roleDesc);
+                // TODO
+                return userDAO.save(user);
+            }
+        }
+        // TODO
+        return new User();
+    }
+
+    public User deleteUser (int userid) {
+        Optional<User> checkUser = userDAO.findById(userid);
+        if (checkUser.isPresent()) {
+            userDAO.deleteById(userid);
+
+            // TODO
+            return (checkUser.get());
+        }
+        // TODO
+        return new User();
+    }
 }
