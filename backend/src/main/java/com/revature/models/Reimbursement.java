d31287e635ae44ae0c8d7bb1d682d331d6384448
Neil Crum <nhc.crum@outlook.com>
2024-04-27 11:26:00 -0700

    Completed reimbursement model

--- backend/src/main/java/com/revature/models/Reimbursement.java
+++ backend/src/main/java/com/revature/models/Reimbursement.java
@@ -1,4 +1,88 @@
 package com.revature.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name = "reimbursements")
+@Component
 public class Reimbursement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int reimbId;
+
+    private String description;
+
+    private float amount;
+
+    private String status;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "userId")
+    @JsonBackReference
+    private User user;
+
+    public Reimbursement() {
+    }
+
+    public Reimbursement(int reimbId, String description, float amount, String status, User user) {
+        this.reimbId = reimbId;
+        this.description = description;
+        this.amount = amount;
+        this.status = status;
+        this.user = user;
+    }
+
+    public int getReimbId() {
+        return reimbId;
+    }
+
+    public void setReimbId(int reimbId) {
+        this.reimbId = reimbId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Reimbursement{" +
+                "reimbId=" + reimbId +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", status='" + status + '\'' +
+                ", user=" + user +
+                '}';
+    }
 }
