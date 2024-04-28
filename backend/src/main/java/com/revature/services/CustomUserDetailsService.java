6d6ed9a86f295ec6999b14087620b19fda9e6160
Neil Crum <nhc.crum@outlook.com>
2024-04-28 08:41:54 -0700

    Converted string roles to a list of SimpleGrantedAuthority. Removed unused dependency. Used constructor dependency injection

--- backend/src/main/java/com/revature/services/CustomUserDetailsService.java
+++ backend/src/main/java/com/revature/services/CustomUserDetailsService.java
@@ -4,27 +4,33 @@ import com.revature.daos.UserDAO;
 import com.revature.models.CustomUserDetails;
 import com.revature.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
     private UserDAO userDAO;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    public CustomUserDetailsService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
 
     @Override
     public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userDAO.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
 
-        return new CustomUserDetails(user.getUsername(), "password_hash", true, true, true, true, Collections.emptyList());
+        List<SimpleGrantedAuthority> authorities = Stream.of(user.getRole())
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        return new CustomUserDetails(user.getUsername(), user.getPassword(), true, true, true, true, authorities);
     }
 }
