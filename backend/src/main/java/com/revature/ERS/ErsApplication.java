062f6f57f30c22d0cb333f7e105b5ea1ff57cc98
Neil Crum <nhc.crum@outlook.com>
2024-04-29 23:01:39 -0700

    Removed dotenv-java support

--- backend/src/main/java/com/revature/ERS/ErsApplication.java
+++ backend/src/main/java/com/revature/ERS/ErsApplication.java
@@ -1,6 +1,5 @@
 package com.revature.ERS;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -14,11 +13,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class ErsApplication {
 
 	public static void main(String[] args) {
-		// load .env file and set environment variables
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
 
 		SpringApplication.run(ErsApplication.class, args);
 
