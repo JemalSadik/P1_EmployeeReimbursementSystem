4853739b5a0b3f603a8bf3eb8c41dc0944606512
Neil Crum <nhc.crum@outlook.com>
2024-05-01 12:56:27 -0700

    moved modal state to dashboard

--- frontend/src/Components/Reimbursement/ModalReimbursement.tsx
+++ frontend/src/Components/Reimbursement/ModalReimbursement.tsx
@@ -1,16 +1,11 @@
 import axios from "axios";
-import { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
+import { ReimbursementProps } from "../../Interfaces/ReimbursementPropsInterface";
 
-export const ModalReimbursement: React.FC<any> = ({reimbursement, key}) => {
+export const ModalReimbursement: React.FC<ReimbursementProps> = ({reimbursement, show, onHide}) => {
 
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    let descInput: string = reimbursement.description;
-    let statusInput: string = reimbursement.status;
+    let descInput: string = reimbursement.description ? reimbursement.description : "";
+    let statusInput: string = reimbursement.status ? reimbursement.status : "";
 
     const getDesc = (input: React.ChangeEvent<HTMLTextAreaElement>) => {
         descInput = input.target.value;
@@ -23,14 +18,14 @@ export const ModalReimbursement: React.FC<any> = ({reimbursement, key}) => {
     const updateReimbursement = async () => {
         let checkClose: boolean = false;
         // TODO: add {withCredentials: true} to axios request for session support
-        if (descInput !== reimbursement.description) {
+        if (descInput !== reimbursement.description && descInput !== "") {
             const resp = await axios.patch(`http://localhost:8080/${reimbursement.reimbId}/description`, descInput)
                 .then((resp) => checkClose = true)
                 .catch((error) => {
                     // change checkClose to false to ensure error is displayed
                     checkClose = false;
 
-                    const errorContainer = document.getElementById(`reimbErrorContainer${key}`);
+                    const errorContainer = document.getElementById(`reimbErrorContainer${reimbursement.reimbId}`);
                     
                     if (errorContainer) {
                         let errorEl = document.createElement('p');
@@ -42,14 +37,14 @@ export const ModalReimbursement: React.FC<any> = ({reimbursement, key}) => {
                 });
         }
         // TODO: check if user role is manager first before sending request to update status
-        if (statusInput !== reimbursement.status) {
+        if (statusInput !== reimbursement.status && statusInput !== "") {
             const resp = await axios.patch(`http://localhost:8080/${reimbursement.reimbId}/status`, statusInput)
                 .then((resp) => checkClose = true)
                 .catch((error) => {
                     // change checkClose to false to ensure error is displayed
                     checkClose = false;
 
-                    const errorContainer = document.getElementById(`reimbErrorContainer${key}`);
+                    const errorContainer = document.getElementById(`reimbErrorContainer${reimbursement.reimbId}`);
                     
                     if (errorContainer) {
                         let errorEl = document.createElement('p');
@@ -60,28 +55,30 @@ export const ModalReimbursement: React.FC<any> = ({reimbursement, key}) => {
                     }
                 });
         }
-        setShow(checkClose);
+        if (checkClose) {
+            onHide();
+        }
     }
 
     return (
-        <Modal show={show} onHide={handleClose}>
+        <Modal show={show} onHide={onHide()} >
             <Modal.Dialog>
                 <Modal.Header closeButton>
-                    <Modal.Title >
-                        <h1 id={`reimbursementModal${key}Label`}>Reimbursement</h1>
+                    <Modal.Title id={`reimbursementModal${reimbursement.reimbId}Label`}>
+                        <h1>Reimbursement</h1>
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <div id={`reimbErrorContainer${key}`} className="border border-danger bg-danger-subtle"></div>
+                    <div id={`reimbErrorContainer${reimbursement.reimbId}`} className="border border-danger bg-danger-subtle"></div>
                     <Form>
-                        <Form.FloatingLabel label="Description" controlId={`reimbDescription${key}`}>
+                        <Form.FloatingLabel label="Description" controlId={`reimbDescription${reimbursement.reimbId}`}>
                             {reimbursement.status == "Pending" ? <Form.Control as="textarea" className="textbox" value={reimbursement.description} onChange={getDesc} /> : <Form.Control as="textarea" className="textbox" value={reimbursement.description} disabled />}
                         </Form.FloatingLabel>
-                        <Form.FloatingLabel label="Amount" controlId={`reimbAmount${key}`}>
+                        <Form.FloatingLabel label="Amount" controlId={`reimbAmount${reimbursement.reimbId}`}>
                             <Form.Control type="text" value={`$${reimbursement.amount}`} disabled />
                         </Form.FloatingLabel>
-                        <Form.FloatingLabel label="Status" controlId={`reimbStatus${key}`}>
-                            {reimbursement.status == "Pending" ? <Form.Select aria-label="Reimbursement Status">
+                        <Form.FloatingLabel label="Status" controlId={`reimbStatus${reimbursement.reimbId}`}>
+                            {reimbursement.status == "Pending" ? <Form.Select aria-label="Reimbursement Status" onChange={getStatus}>
                                 <option value="Pending" selected>Pending</option>
                                 <option value="Approved">Approved</option>
                                 <option value="Denied">Denied</option>
@@ -92,7 +89,7 @@ export const ModalReimbursement: React.FC<any> = ({reimbursement, key}) => {
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>Close</Button>
+                    <Button variant="secondary" onClick={onHide()}>Close</Button>
                     <Button variant="primary" onClick={updateReimbursement}>Save</Button>
                 </Modal.Footer>
             </Modal.Dialog>
