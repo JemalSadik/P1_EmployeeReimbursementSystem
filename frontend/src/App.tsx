c65cb39dd7f9d1e895eabcee825799ef98efe31d
Neil Crum <nhc.crum@outlook.com>
2024-05-01 12:59:08 -0700

    set baseURL in localStorage, import bootstrap and bootstrap-icons styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './Components/Landing/Login';
 import { Register } from './Components/Landing/Register';
 import { Dashboard } from './Components/Dashboard/Dashboard';
 
 function App() {
+
+    localStorage.setItem("baseUrl", "http://localhost:8080")
+
     return (
         <div className="App">
             <BrowserRouter>
