59753df8509e333a30217609ce5fcd5cc9d07b8c
Neil Crum <nhc.crum@outlook.com>
2024-05-01 12:59:44 -0700

    Add reimbursement component

--- frontend/src/Components/Reimbursement/Reimbursement.tsx
+++ frontend/src/Components/Reimbursement/Reimbursement.tsx
@@ -1,11 +1,14 @@
 import { ReimbursementInterface } from "../../Interfaces/ReimbursementInterface"
 
-export const Reimbursement: React.FC = (reimbursement: ReimbursementInterface) => {
+export const Reimbursement: React.FC<any> = (reimbursement: ReimbursementInterface, showModal: CallableFunction) => {
 
 
     return (
-        <div>
-
-        </div>
+        <tr onClick={showModal(reimbursement)}>
+            <th>{reimbursement.reimbId}</th>
+            <td>{reimbursement.description}</td>
+            <td>{reimbursement.amount}</td>
+            <td>{reimbursement.status}</td>
+        </tr>
     )
 }
