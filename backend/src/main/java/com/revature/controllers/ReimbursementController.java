27e0ea5994c5bc75518ae255f8107d5226ee586a
Neil Crum <nhc.crum@outlook.com>
2024-05-01 21:30:26 -0700

    Update to use session and add checks for logged-in user and user role

--- backend/src/main/java/com/revature/controllers/ReimbursementController.java
+++ backend/src/main/java/com/revature/controllers/ReimbursementController.java
@@ -3,6 +3,7 @@ package com.revature.controllers;
 import com.revature.models.Reimbursement;
 import com.revature.models.dtos.IncomingReimbursementDTO;
 import com.revature.services.ReimbursementService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,23 +24,39 @@ public class ReimbursementController {
 
     // get all reimbursements (manager)
     @GetMapping
-    public ResponseEntity<List<Reimbursement>> getAllReimbursements() {
+    public ResponseEntity<?> getAllReimbursements(HttpSession session) {
+        if (session.getAttribute("userId") == null) {
+            return ResponseEntity.status(401).body("You must be logged in to see all reimbursements");
+        }
         List<Reimbursement> reimbursements = reimbService.getAllReimbursements();
-        // TODO: catch invalid user role error and return a 403 status code
+        String role = (String) session.getAttribute("role");
+        if (!role.equalsIgnoreCase("Manager")) {
+            return ResponseEntity.status(403).body("You must be a MANAGER to see all reimbursements");
+        }
         return ResponseEntity.ok(reimbursements);
     }
 
     // get all pending reimbursements (manager)
     @GetMapping("/pending")
-    public ResponseEntity<List<Reimbursement>> getAllPendingReimbursements() {
+    public ResponseEntity<?> getAllPendingReimbursements(HttpSession session) {
+        if (session.getAttribute("userId") == null) {
+            return ResponseEntity.status(401).body("You must be logged in to see all pending reimbursements");
+        }
+        String role = (String) session.getAttribute("role");
+        if (!role.equalsIgnoreCase("Manager")) {
+            return ResponseEntity.status(403).body("You must be a MANAGER to see all pending reimbursements");
+        }
         List<Reimbursement> pendingReimb = reimbService.getAllPendingReimbursement();
-        // TODO: catch invalid user role error and return a 403 status code
         return ResponseEntity.ok(pendingReimb);
     }
 
     // get all reimbursements by user ID (user)
-    @GetMapping("/users/{userId}")
-    public ResponseEntity<Object> getAllReimbursementsByUserId(@PathVariable int userId) {
+    @GetMapping("/users")
+    public ResponseEntity<?> getAllReimbursementsByUserId(HttpSession session) {
+        if (session.getAttribute("userId") == null) {
+            return ResponseEntity.status(401).body("You must be logged in to see all your reimbursements");
+        }
+        int userId = (int) session.getAttribute("userId");
         try {
             List<Reimbursement> userReimbursements = reimbService.getAllReimbursementsByUserId(userId);
             return ResponseEntity.ok(userReimbursements);
@@ -49,8 +66,12 @@ public class ReimbursementController {
     }
 
     // get all pending reimbursements for user ID (user)
-    @GetMapping("/users/{userId}/pending")
-    public ResponseEntity<Object> getAllPendingReimbursementsByUserId(@PathVariable int userId) {
+    @GetMapping("/users/pending")
+    public ResponseEntity<?> getAllPendingReimbursementsByUserId(HttpSession session) {
+        if (session.getAttribute("userId") == null) {
+            return ResponseEntity.status(401).body("You must be logged in to see all your pending reimbursements");
+        }
+        int userId = (int) session.getAttribute("userId");
         try {
             List<Reimbursement> pendingUserReimbursements = reimbService.getAllPendingReimbursementsByUserId(userId);
             return ResponseEntity.ok(pendingUserReimbursements);
@@ -60,8 +81,12 @@ public class ReimbursementController {
     }
 
     // Create a new reimbursement for a user ID (user)
-    @PostMapping("/users/{userId}")
-    public ResponseEntity<Object> createReimbursement(@RequestBody IncomingReimbursementDTO reimbursementDTO, @PathVariable int userId) {
+    @PostMapping("/users")
+    public ResponseEntity<Object> createReimbursement(@RequestBody IncomingReimbursementDTO reimbursementDTO, HttpSession session) {
+        if (session.getAttribute("userId") == null) {
+            return ResponseEntity.status(401).body("You must be logged in to create a reimbursement");
+        }
+        int userId = (int) session.getAttribute("userId");
         // Set user ID
         reimbursementDTO.setUserId(userId);
         // Set status to "Pending"
@@ -76,14 +101,20 @@ public class ReimbursementController {
 
     // update a reimbursement status by reimbursement ID (manager)
     @PatchMapping("/{reimbId}/status")
-    public ResponseEntity<Object> updateReimbursementStatus(@RequestBody String status, @PathVariable int reimbId) {
+    public ResponseEntity<Object> updateReimbursementStatus(@RequestBody String status, @PathVariable int reimbId, HttpSession session) {
+        if (session.getAttribute("userId") == null) {
+            return ResponseEntity.status(401).body("You must be logged in to update a reimbursement's status");
+        }
+        String role = (String) session.getAttribute("role");
+        if (!role.equalsIgnoreCase("Manager")) {
+            return ResponseEntity.status(403).body("You must be a MANAGER to update a reimbursement's status");
+        }
         try {
             Reimbursement reimb = reimbService.updateReimbursementStatus(reimbId, status);
             return ResponseEntity.ok(reimb);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(400).body(e.getMessage());
         }
-        // TODO: catch invalid user role error and return a 403 status code
     }
 
     /**
@@ -93,7 +124,10 @@ public class ReimbursementController {
      * @return ResponseEntity with the updated reimbursement or appropriate error message
      */
     @PatchMapping("/{reimbId}/description")
-    public ResponseEntity<Object> updatePendingReimbursementDescription(@RequestBody String description, @PathVariable int reimbId) {
+    public ResponseEntity<Object> updatePendingReimbursementDescription(@RequestBody String description, @PathVariable int reimbId, HttpSession session) {
+        if (session.getAttribute("userId") == null) {
+            return ResponseEntity.status(401).body("You must be logged in to update a reimbursement's description");
+        }
         try {
             Reimbursement reimb = reimbService.updatePendingReimbursementDescription(reimbId, description);
             return ResponseEntity.ok(reimb);
