a3491ac2209ebe0349a31f8e54c81124da334bff
Neil Crum <nhc.crum@outlook.com>
2024-05-02 19:22:05 -0700

    Pass userId when returning outgoing user DTO

--- backend/src/main/java/com/revature/controllers/AuthenticationController.java
+++ backend/src/main/java/com/revature/controllers/AuthenticationController.java
@@ -35,7 +35,7 @@ public class AuthenticationController {
         session.setAttribute("username", user.getUsername());
         session.setAttribute("role", user.getRole());
 
-        return ResponseEntity.ok(new OutgoingUserDTO(user.getUsername(), user.getFirstname(), user.getLastname(), user.getRole()));
+        return ResponseEntity.ok(new OutgoingUserDTO(user.getUserId(), user.getUsername(), user.getRole()));
     }
 
     @PostMapping("/logout")
